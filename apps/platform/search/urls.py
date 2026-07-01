b33c20a1f7507ca61d0171120fc1737f3d1db88f
prthm2910 <pratham.kubasad2004@gmail.com>
2026-07-01 14:32:21 +0530

    refactor: move search/ prefix from search app to main urlconf

--- apps/platform/search/urls.py
+++ apps/platform/search/urls.py
@@ -7,5 +7,5 @@ from django.urls import path
 from .views import GlobalSearchView
 
 urlpatterns = [
-    path("search/", GlobalSearchView.as_view(), name="global-search"),
+    path("", GlobalSearchView.as_view(), name="global-search"),
 ]
